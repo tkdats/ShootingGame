b4be1111bb76fecfcdd545c1fe6903df79dac261
K^2 <K^2@k2-PC>
2017-04-27 18:01:58 +0900

    雑魚キャラ変更

--- EnemyGenerater/EnemyGenerater.java
+++ EnemyGenerater/EnemyGenerater.java
@@ -5,9 +5,6 @@ public class EnemyGenerater {
 	Random rnd=new Random();
 	public void generate(GameInfo gameinfo, LinkedList<Enemy> _enemies) {
 		//10フレーム1回SampleEnemy1を出す。
-		if(gameinfo.frames%10==0){
-			_enemies.add(new SampleEnemy1((double)rnd.nextInt(Utility.sizex),1,(double)(rnd.nextInt(10)-5),3));
-		}
 		if(gameinfo.frames==1000){
 			_enemies.add(new Boss1(250,25,0,0));
 		}
